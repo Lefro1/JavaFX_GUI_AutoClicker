1149c2f891af4df9d899450321eff5f34ef568b1
jasdrive <65259917+jasdrive@users.noreply.github.com>
2022-05-15 13:31:59 -0700

    Implemented logic for MenuButtons and MenuItems on selecting mouse button and click multiple.

--- src/GUI/GUIController.java
+++ src/GUI/GUIController.java
@@ -2,6 +2,7 @@ package GUI;
 
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
@@ -35,28 +36,46 @@ public class GUIController implements Initializable {
     public Button stopButton;
     public Button setHotkeyButton;
 
+    public MenuButton mouseButtonMenuButton;
+    public MenuButton clickMultipleMenuButton;
+
+    private final MenuItem menuItemLeftClick = new MenuItem("LeftClick");
+    private final MenuItem menuItemMiddleClick = new MenuItem("MiddleClick");
+    private final MenuItem menuItemRightClick = new MenuItem("RightClick");
+    private final MenuItem menuItemTripleClick = new MenuItem("TripleClick");
+    private final MenuItem menuItemSingleClick = new MenuItem("SingleClick");
+    private final MenuItem menuItemDoubleClick = new MenuItem("DoubleClick");
+
+
     private int minuteClickDelaySpinnerValue;
     private int secondsClickDelaySpinnerValue;
     private int millisecondsClickDelaySpinnerValue;
     private int clicksPerSecondSpinnerValue;
     private int totalClicksSpinnerValue;
 
+    private String mouseButtonString;
+    private String clickMultipleString;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         // Creating a SpinnerValueFactory to handle the aspects of the Spinner objects. Allowing ints of 0 to 999
-        // We do not need numbers larger than this. No support is provided for a click rate less than 1 click/999 minutes.
-        // It is not possible with Java to have a sleep time less than 1 ms, so 1,000 is the max theoretical clicks per second.
         setupSpinnerValueFactories();
 
-        // Adding the change listeners to update int fields as changes occur.
+        // Adding the change listeners to update int fields as changes occur. Without this, local values will not update.
         addChangeListenersToSpinners();
+
+        // Initialize the menu buttons to have selection options.
+        initializeMenuItems();
     }
 
 
-
+    /**
+     * Each spinner needs its own unique ValueFactory, otherwise the values will change with each other.
+     * Setting up all value selection spinners to accept ints ranging from 0 to 999
+     * We do not need numbers larger than this. No support is provided for a click rate less than 1 click/999 minutes.
+     * It is not possible with Java to have a sleep time less than 1 ms, so 1,000 is the max theoretical clicks per second.
+     */
     private void setupSpinnerValueFactories() {
-
-        // Every spinner needs its own ValueFactory, otherwise the values will change with each other.
         minuteClickDelaySpinner.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(0,999));
         secondsClickDelaySpinner.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(0,999));
         millisecondsClickDelaySpinner.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(0,999));
@@ -67,6 +86,9 @@ public class GUIController implements Initializable {
     /**
      * Adds a listener to every specified Spinner field.
      * This listener updates the int fields of the respective {@literal <Spinner>}Value
+     *
+     * Currently is lacking regex check for non-numeric values {@code if(!newValue.matches("\\d*"))}
+     * This will allow users to enter invalid entries, but program will not run with text or decimals in fields.
      */
     private void addChangeListenersToSpinners() {
         minuteClickDelaySpinner.valueProperty().addListener(new ChangeListener<Integer>() {
@@ -98,6 +120,58 @@ public class GUIController implements Initializable {
             public void changed(ObservableValue<? extends Integer> observable, Integer oldValue, Integer newValue) {
                 totalClicksSpinnerValue = totalClicksSpinner.getValue();
             }
+        });
+    }
+
+
+    /**
+     * Adds all MenuItems to their respective MenuButtons.
+     * Sets the "On Action" of the MenuItems within the MenuButton to update the Button and assign the choice.
+     *
+     * In use, when "LeftClick" is selected, it updates the MenuButton to display that, and re-assigns the internal
+     * String "mouseButtonString" to be used in other methods, tracking the current state.
+     */
+    private void initializeMenuItems() {
+        // Adding MenuItems to MenuButtons
+        mouseButtonMenuButton.getItems().addAll(menuItemLeftClick, menuItemMiddleClick, menuItemRightClick);
+        clickMultipleMenuButton.getItems().addAll(menuItemSingleClick, menuItemDoubleClick, menuItemTripleClick);
+
+        // Defining defaults of MenuButtons on startup.
+        mouseButtonMenuButton.setText(menuItemLeftClick.getText());
+        mouseButtonString = menuItemLeftClick.getText();
+
+        clickMultipleMenuButton.setText(menuItemSingleClick.getText());
+        clickMultipleString = menuItemSingleClick.getText();
+
+        // Assigning the "onAction" of all MenuItems so when selected, values update.
+        menuItemLeftClick.setOnAction(e -> {
+            mouseButtonMenuButton.setText(menuItemLeftClick.getText());
+            mouseButtonString = menuItemLeftClick.getText();
+        });
+
+        menuItemMiddleClick.setOnAction(e -> {
+            mouseButtonMenuButton.setText(menuItemMiddleClick.getText());
+            mouseButtonString = menuItemMiddleClick.getText();
+        });
+
+        menuItemRightClick.setOnAction(e -> {
+            mouseButtonMenuButton.setText(menuItemRightClick.getText());
+            mouseButtonString = menuItemRightClick.getText();
+        });
+
+        menuItemSingleClick.setOnAction(e -> {
+            clickMultipleMenuButton.setText(menuItemSingleClick.getText());
+            clickMultipleString = menuItemSingleClick.getText();
+        });
+
+        menuItemDoubleClick.setOnAction(e -> {
+            clickMultipleMenuButton.setText(menuItemDoubleClick.getText());
+            clickMultipleString = menuItemDoubleClick.getText();
+        });
+
+        menuItemTripleClick.setOnAction(e -> {
+            clickMultipleMenuButton.setText(menuItemTripleClick.getText());
+            clickMultipleString = menuItemTripleClick.getText();
         });
     }
 
